c88378f3ff7d7de3f7201ed69df553c677a773b2
defrindr <defrindr@gmail.com>
2020-09-06 16:17:14 +0700

    :tada: add error handler

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./src/brainly.js');
+/**
+ * Defri Indra Mahardika
+ * Updated on 4 April 2020
+ * 
+ */
+const Brainly = require('./src/brainly.js');
+
+const brainly = async(query, count=5) => {
+    let response = await Brainly(query, count).catch(error => {
+        return {
+            'success': false,
+            'message': error,
+        };
+    });
+
+    return response;
+};
+
+module.exports = brainly;
